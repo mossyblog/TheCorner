f7abd0561dcb12a1b0c8320d2a77c29886774937
Scott <scott.barnes@gmail.com>
2023-01-31 22:37:13 +1000

    Default State

---
+++ Source/TheCorner/AI/AAINPC.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AAINPC.h"
+
+#include "MainStructs.h"
+#include "TheCorner/TheCornerGameMode.h"
+
+AAINPC::AAINPC()
+{
+}
+
+void AAINPC::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// We need to spawn some subjects initially to perform the logic on
+	for (int i = 0; i < NumSubjects; i++)
+	{
+		// Create a new subject and save a refrence to its handle
+		// An FSubjectHandle is the main way you're going to interface with Apparatus when adding/removing data from a subject
+		FSubjectHandle NewSubject = SpawnSubject();
+
+		// Create a new position trait for this subject
+		// This is a custom made NON Apparatus struct that you will have to create for your own project
+		// It is defined in MainStructs.h
+		FSubjectPosition NewPosition;
+
+		// Set its X/Y randomly
+		NewPosition.Value = FVector(FMath::RandRange(-RandomWidth, RandomWidth),
+		                            FMath::RandRange(-RandomWidth, RandomWidth), 0);
+
+		// In addition, lets give our subject a destination so that it can randomly walk around
+		// Much like our Position struct, this is another custom struct ...
+		FDestination NewDestination;
+		NewDestination.Value = FVector(FMath::RandRange(-RandomWidth, RandomWidth),
+		                               FMath::RandRange(-RandomWidth, RandomWidth), 0);
+
+		// Add the position trait to the subject
+		// Calling .SetTrait also adds the trait if it doesn't already exist.
+		NewSubject.SetTrait(NewPosition);
+
+		// Add the destination trait to the subject
+		NewSubject.SetTrait(NewDestination);
+
+		// Thats it! We created a new subject, and gave it a random position / destination
+
+		UE_LOG(LogTemp, Warning, TEXT("Spawning...: %i"), i);
+	}
+
+}
+
+
+void AAINPC::Tick(const float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	// Filter for all actors that have a movement component
+	FFilter Filter = FFilter::Make<FDestination>();
+	TSharedRef<TChain<TChunkIt<FSubjectHandle>, TBeltIt<FSubjectHandle>, EParadigm::Safe>> SolidChain = Enchain(Filter);
+}
---
+++ Source/TheCorner/AI/AAINPC.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ApparatusRuntime.h"
+#include "Mechanism.h"
+#include "MechanicalActor.h"
+#include "AAINPC.generated.h"
+/**
+ * 
+ */
+UCLASS()
+class THECORNER_API AAINPC : public AMechanicalActor
+{
+	GENERATED_BODY()
+	
+	virtual void Tick(const float DeltaTime) override;
+	virtual void BeginPlay() override;
+public:
+	AAINPC();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="MoveRandomly")
+	float MinDistanceFroMDestination = 10.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="MoveRandomly")
+	float MoveSpeed  = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="MoveRandomly")
+	float RandomWidth  = 250.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="MoveRandomly")
+	int NumSubjects  = 10;
+};
